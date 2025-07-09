497f24cde271ebefac2cda304cb924bff01a46ed
Gokhan Gunduz <gokhangndz@gmail.com>
2025-07-09 19:21:38 +0300

    add logrotate and rsyslog to haproxy

--- scripts/haproxy.sh
+++ scripts/haproxy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+apt-get update
 apt-get install -y haproxy
 
 cat <<EOF > /etc/haproxy/haproxy.cfg
@@ -26,6 +27,31 @@ backend kubernetes-masters
     server cp3 192.168.56.13:6443 check
 EOF
 
+# logrotate for haproxy
+cat <<EOF > /etc/logrotate.d/haproxy
+/var/log/haproxy.log {
+    daily         # Rotate logs daily
+    rotate 7      # Keep 7 rotated log files
+    compress      # Compress the rotated log files
+    delaycompress # Delay compression until the next rotation cycle
+    missingok     # Don't exit with error if the log file is missing
+    notifempty    # Don't rotate if the log file is empty
+    create 0640 haproxy adm # Create a new empty log file with specified permissions and ownership
+    sharedscripts # Only run postrotate script once for all log files in the block
+    postrotate    # Script to run after rotation
+        # Instruct rsyslogd (or syslog-ng) to reopen its log files
+        # This is crucial so HAProxy starts writing to the new, empty file
+        invoke-rc.d rsyslog reload >/dev/null 2>&1 || true
+    endscript
+}
+EOF
+
+# rsyslog for haproxy logs, becuz of to separate log system
+apt-get install -y rsyslog
+systemctl restart rsyslog
+systemctl enable rsyslog
+
+# haproxy service start-enable
 systemctl restart haproxy
 systemctl enable haproxy
 
